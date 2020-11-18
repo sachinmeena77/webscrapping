020f66cad289e554e05d322148888db94b8ae951
detraxsenpaii <krrish.meena77@gmail.com>
2020-11-18 22:45:51 +0530

    done tille post_titles things

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,10 @@
 import requests
+from urllib.parse import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
+
+BASE_CRAIGSLIST_URL = 'https://ahmedabad.craigslist.org/d/for-sale/search/sss?query={}'
 
 
 # Create your views here.
@@ -10,6 +14,13 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
-    stuff_for_front_end = {'search': search}
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class': 'result-title'})
+    print(post_titles[0])
+    stuff_for_front_end = {'search': search, }
     return render(request, 'my_app/new_search.html', stuff_for_front_end)
