7883704266f612774ad217b6fefa9411fcc371cb
detraxsenpaii <krrish.meena77@gmail.com>
2020-11-18 19:38:19 +0530

    done front end work!

--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_front_end = {'search': search}
+    return render(request, 'my_app/new_search.html', stuff_for_front_end)
