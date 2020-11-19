af94efb0476d35f218d3a22447b9001717958526
detraxsenpaii <krrish.meena77@gmail.com>
2020-11-19 12:25:02 +0530

    completed work!

--- my_app/views.py
+++ my_app/views.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from . import models
 
 BASE_CRAIGSLIST_URL = 'https://ahmedabad.craigslist.org/d/for-sale/search/sss?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 # Create your views here.
@@ -16,11 +17,31 @@ def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
-    print(final_url)
     response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
-    post_titles = soup.find_all('a', {'class': 'result-title'})
-    print(post_titles[0])
-    stuff_for_front_end = {'search': search, }
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_posting = []
+
+    for post in post_listings:
+        post_titles = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'NA'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_posting.append((post_titles, post_url, post_price, post_image_url))
+
+    stuff_for_front_end = {'search': search,
+                           'final_posting': final_posting}
     return render(request, 'my_app/new_search.html', stuff_for_front_end)
